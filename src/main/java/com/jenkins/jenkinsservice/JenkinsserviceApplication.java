ee86e2b1295c1175f3de81028ae0ea35574951d5
Kukeit Chanahan <Kukeit.C@gmail.com>
2021-08-30 16:40:08 +0700

    first commit

---
+++ src/main/java/com/jenkins/jenkinsservice/JenkinsserviceApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.jenkinsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jenkins/jenkinsservice/JenkinsserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.jenkinsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
